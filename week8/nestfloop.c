4041b21b929563b6be9e152ea620387e8e65ab87
yy13579242223 <imemily093@gmail.com>
2024-11-08 14:45:51 +0000

    week8

--- week8/nestfloop.c
+++ week8/nestfloop.c
@@ -19,15 +19,61 @@ int main(void){
         puts ("");
     }
 
-    printf("enter row:");
+    printf("Enter your seat row number:");
     scanf("%d", &myR);
-    printf("enter column:");
+    printf("Enter your seat column number:");
     scanf("%d", &myC);
 
     for(row=0;row<1;row++){
     puts("   C1  C2  C3  C4  C5");
     }
     for(row=1; row<=myrow; row++){
+
+        for(column=1; column<=mycolumn; column++){
+            if(column==1){
+              printf("R%u ", row);
+            }
+            if(row==myR && column==myC){
+                printf("X   ");
+            }
+            else{
+            printf("*   ");
+            }
+        }
+        puts ("");
+    }
+    printf("X=R%d, C%d\n", myR, myC);
+    puts("Confirm seat[1/0]:");
+    scanf(" %c", &confirm);
+    getchar();
+
+    if(confirm == '1'){
+        printf("Thank you for booking your seat.\n");
+        }
+    while(confirm == '0'){
+        for(row=0;row<1;row++){
+        puts("   C1  C2  C3  C4  C5");
+        }
+        for(row=1; row<=myrow; row++){
+
+        for(column=1; column<=mycolumn; column++){
+            if(column==1){
+              printf("R%u ", row);
+            }
+            printf("*   ");
+        }
+        puts ("");
+         }
+
+        printf("Enter your seat row number:");
+        scanf("%d", &myR);
+        printf("Enter your seat column number:");
+        scanf("%d", &myC);
+
+        for(row=0;row<1;row++){
+        puts("   C1  C2  C3  C4  C5");
+        }
+        for(row=1; row<=myrow; row++){
 
         for(column=1; column<=mycolumn; column++){
             if(column==1){
@@ -41,5 +87,14 @@ int main(void){
             }
         }
         puts ("");
+         }
+        printf("X=R%d, C%d\n", myR, myC);
+        puts("Conferm seat[1/0]:");
+        scanf(" %c", &confirm);
+        getchar();
+        if(confirm == '1'){
+          printf("Thank you for booking your seat.\n");
+        }
     }
+    
 }//endmain
